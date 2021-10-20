c9b1bbe716355df60152a5d55b41897b9b41181e
Carl Furrow <carl.furrow@betterup.co>
2021-10-20 10:21:41 -0400

    Verify that .env.test will override values set in .env

--- spec/dotenv/railtie_spec.rb
+++ spec/dotenv/railtie_spec.rb
@@ -5,13 +5,21 @@ describe Dotenv::Beefy::Railtie do
     let(:instance) { described_class.instance }
     subject(:load_environments) { instance.load_environments }
 
-    it 'loads .env' do
+    def mock_dotenv_file
       contents = <<-FILE
       FOO=bar
       BAZ=buzz
       ANSWER=42
       FILE
       mock_file_read(filename: '.env', contents: contents)
+    end
+
+    before do
+      initialize_mock_file_reads
+    end
+
+    it 'loads .env' do
+      mock_dotenv_file
       load_environments
 
       expect(ENV['FOO']).to eq('bar')
@@ -19,7 +27,24 @@ describe Dotenv::Beefy::Railtie do
       expect(ENV['ANSWER']).to eq('42')
     end
 
-    it 'loads .env.test and .env'
+    it 'overrides .env values with .env.test values' do
+      mock_dotenv_file
+
+      contents = <<-FILE
+      FOO=bizz
+      BAZ=farf
+      ANSWER=99
+      FILE
+      mock_file_read(filename: '.env.test', contents: contents)
+      
+      load_environments
+
+      expect(ENV['FOO']).to eq('bizz')
+      expect(ENV['BAZ']).to eq('farf')
+      expect(ENV['ANSWER']).to eq('99')
+    end
+
+
     it 'overrides .env values with more specific .env.test values'
     it 'loads values from .env, .env.test and .env.test.local files'
   end
