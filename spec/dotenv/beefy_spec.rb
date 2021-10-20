7e11704cdb98cf45a5a6d0ddf0a3a3fa7567447f
Carl Furrow <carl.furrow@betterup.co>
2021-10-20 10:07:08 -0400

    Remove placeholder spec example

--- spec/dotenv/beefy_spec.rb
+++ spec/dotenv/beefy_spec.rb
@@ -4,8 +4,4 @@ describe Dotenv::Beefy do
   it 'has a version number' do
     expect(Dotenv::Beefy::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
